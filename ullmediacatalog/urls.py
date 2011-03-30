3f0c1978f3f6aa78a12e61c8a03656e47bd3bc43
David Cabrera <dcabrera@dpcat.es>
2011-03-30 13:21:04 +0000

    - Creadas las carpetas que contendrán los ficheros css e imágenes en static - Añadido fichero base.css e imágenes del login.html - Modificado el html del login.html - Creado el fichero base.html que contrendrá la estructura básica de html

--- ullmediacatalog/urls.py
+++ ullmediacatalog/urls.py
@@ -10,5 +10,6 @@ urlpatterns = patterns('',
     (r'^postproduccion/', include('postproduccion.urls')),
     (r'^admin/', include(admin.site.urls)),
     (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    #(r'^files/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT,'show_indexes':True}),
     (r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name' : 'postproduccion/login.html'}),
 )
