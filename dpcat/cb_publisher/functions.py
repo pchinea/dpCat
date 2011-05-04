51f675efb23e671de812d9d053d185690c143994
Pablo Chinea <pchinea@dpcat.es>
2011-05-04 12:45:44 +0000

    Creada la versión inicial de la aplicación de publicación en ClipBucket.

---
+++ dpcat/cb_publisher/functions.py
@@ -0,0 +1,22 @@
+# encoding: utf-8
+from django.shortcuts import render_to_response
+
+import subprocess
+
+def get_uploader_code(v):
+    cb_path = '/var/www/clipbucket'
+    auth = { 'user' : 'admin', 'pass' : 'JXkru6' }
+    return render_to_response('cb_publisher/uploader.php', { 'v' : v, 'cb_path' : cb_path, 'auth' : auth }).content
+
+def execute_upload(v):
+    php_path = '/usr/bin/php'
+
+    p = subprocess.Popen(php_path, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    messages = p.communicate(input=get_uploader_code(v))[0]
+
+    if p.returncode == 0:
+        print "Todo OK"
+    else:
+        print "Error:"
+        print messages
+
---
+++ dpcat/templates/cb_publisher/uploader.php
@@ -0,0 +1,43 @@
+<?php
+
+// Indica al ClipBucket que se trata de una operación en background (no enviada
+// desde el navegador), debe hacerse antes de incluir el ClipBucket.
+$in_bg_cron = true;
+
+include("{{ cb_path}}/includes/config.inc.php");
+
+// Prepara la información sobre el vídeo a publicar.
+$file_key = time() . RandomString(5);
+$file_src = '{{ v.fichero }}';
+$array = array(
+    'title' => '{{ v.metadata.titulo }}',
+    'description' => '{{ v.metadata.descripcion }}',
+    'tags' => 'PENDIENTE,ATENCIÓN', # TODO: Extraer los tags de los metadatos cuando estén.
+    'category' => array(1),
+    'file_name' => $file_key
+);
+
+// Se autentica contra el ClipBucket.
+global $userquery;
+$userquery->login_user("{{ auth.user }}", "{{ auth.pass }}");
+
+// Se le envía al ClipBucket los datos del vídeo.
+$upl = new Upload();
+$vid = $upl->submit_upload($array);
+
+// Datos aceptados, se copia el vídeo y se inserta en la cola de procesado.
+if ($vid) {
+    $base_name = $file_key . '.' . getExt($file_src);
+    $file_name = TEMP_DIR . '/' . $base_name;
+    copy($file_src, $file_name);
+    $upl->add_conversion_queue($base_name);
+    exit(0);
+}
+
+// Error: No se pudo publicar el vídeo.
+global $eh;
+foreach ($eh->error_list as $err)
+    echo "$err\n";
+exit(1);
+
+?>
