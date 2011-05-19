964915e9e8ea6f70ff020c574a429f53e1207816
Pablo Chinea <pchinea@dpcat.es>
2011-05-19 11:19:49 +0000

    Cambiados los datos de conexión con la base de datos.

--- dpcat/settings.py
+++ dpcat/settings.py
@@ -14,8 +14,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'dpcat',                      # Or path to database file if using sqlite3.
-        'USER': 'udv',                      # Not used with sqlite3.
-        'PASSWORD': 'JXkru6',                  # Not used with sqlite3.
+        'USER': 'dpcatuser',                      # Not used with sqlite3.
+        'PASSWORD': 'dpcat1234',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
