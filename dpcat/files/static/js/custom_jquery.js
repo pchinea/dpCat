9b3e5dfdca77e9db29073b70e44eb32068d4576a
David Cabrera <dcabrera@dpcat.es>
2011-05-24 17:16:24 +0000

    Cambios en css de videoteca.
    Cambio en la manera con la que se embeben los widget, ahora siempre por defecto esta el widget de opciones de monitorización para que sean accesible en todo momento.
    
    Se han borrado los enlaces inferiores que se usaban de debug.

--- dpcat/files/static/js/custom_jquery.js
+++ dpcat/files/static/js/custom_jquery.js
@@ -17,7 +17,32 @@ $(document).ready(function() {
         $(this.parentNode.parentNode).find(".form-validacion-rechaza").toggle();
     });
 
-    
+    $(".videoteca-buscador-header").click(function () {
+        $(this.parentNode.parentNode).find(".videoteca-buscador-content").toggle();
+    });
+
+   
+
+    $(function() {
+        var dates = $( "#from, #to" ).datepicker({
+            defaultDate: "+1w",
+            changeMonth: true,
+            numberOfMonths: 3,
+            onSelect: function( selectedDate ) {
+                var option = this.id == "from" ? "minDate" : "maxDate",
+                    instance = $( this ).data( "datepicker" ),
+                    date = $.datepicker.parseDate(
+                        instance.settings.dateFormat ||
+                        $.datepicker._defaults.dateFormat,
+                        selectedDate, instance.settings );
+                dates.not( this ).datepicker( "option", option, date );
+           }
+        });
+    });
+
+
+
+
 });
 
 
